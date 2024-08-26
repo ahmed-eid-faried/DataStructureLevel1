d91aab9b235e5672157b6c40e78b84c5c2d41fd7
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-08-26 13:36:57 +0300

    Add project files.

---
+++ DataStructureLevel1/DataStructureLevel1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Data Strucure\n";
+    //ClassificationOfDataStructure();
+    //BigOofOne();
+    //BigOofLogN();
+    //BigOofN();
+    //BigOofNLogN();
+    //BigOofN2();
+    //BigOofN3();
+    //BigOof2PowN();
+    //BigOofClobberedN();
+    //CalculationBigO();
+    //BinaryAsDataStructure();
+    //ArrayEx();
+    //MatrixEx();
+    //StackEx();
+    //VectorEx();
+    //QueueEx();
+    //SinglyLinkedListEx();
+    //DoublyLinkedListEx();
+    //CircularLinkedListEx();
+    //ADTEx();
+    //MapEx();
+    //UnionEx();
+}
+ 
